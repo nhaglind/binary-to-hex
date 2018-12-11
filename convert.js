dcb005500d663f76f87dfed706ad652e97386e75
Nick Haglind <nhaglind@gmail.com>
2018-12-11 13:47:17 -0600

    add checking for invalid

--- convert.js
+++ convert.js
@@ -3,16 +3,32 @@ window.onload=function(){
   let result = document.querySelector('#result').value;
   function binary_to_hex (element) {
     let binary = document.querySelector('#binary').value;
+    if(binary.search(/^[0-1]*$/) == -1){
+      document.querySelector("#binary").style.color = "red";
+      document.querySelector("#result").style.visibility = "hidden";
+    } else
+    {
+      document.querySelector("#binary").style.color = "white";
+      document.querySelector("#result").style.visibility = "visible";
+    }
     binary = String(binary);
     // let binary = '01000110101000101011100100111101';
     let group = binary.match(/.{1,4}/g);
+    let pad  = "0000";
+    for (let a = 0; a < group.length; a++) {
+      group[a] = (pad + group[a]).slice(-pad.length);
+    }
+
+    console.log(group);
+
     let hex_a = [];
     for (let i = 0; i < group.length; i++) {
-      var w = parseInt(group[i][0]) * 8;
-      var x = parseInt(group[i][1]) * 4;
-      var y = parseInt(group[i][2]) * 2;
-      var z = parseInt(group[i][3]) * 1;
-      var t = parseInt(w + x + y + z);
+      var w, x, y, z = 0;
+      w = parseInt(group[i][0]) * 8;
+      x = parseInt(group[i][1]) * 4;
+      y = parseInt(group[i][2]) * 2;
+      z = parseInt(group[i][3]) * 1;
+      t = parseInt(w + x + y + z);
       hex_a.push(t);
       console.log(hex_a);
     }
